e3b7c68c772b7a6d9b2d15844fb2988c89d7c417
Ryan Brink <ryanbrink@Ryans-MacBook-Pro-7.local>
2014-03-13 08:23:11 -0400

    Initial project setup

---
+++ Hey Jane/Hey Jane/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hey Jane
+//
+//  Created by Ryan Brink on 2014-03-13.
+//  Copyright (c) 2014 HeyJane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HJAppDelegate class]));
+    }
+}
